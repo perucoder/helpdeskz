48d258bc757ef302f39e6ed3398c356d64d2675d
andres <alvaritusg@gmail.com>
2017-09-09 02:25:02 -0500

    Update Imap Library

--- imap.php
+++ imap.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php -q
 <?php
 define('UPLOAD_DIR','uploads/');
-define('MINUTES_CHECK', 3);
+define('MINUTES_CHECK', 5);
 
 include __DIR__.DIRECTORY_SEPARATOR.'init.php';
 if ( ! function_exists('imap_open'))

72c8587d886b94de0bfd17ddf1c4566293b55ef9
andres <alvaritusg@gmail.com>
2017-09-09 02:48:37 -0500

    A new change

--- init.php
+++ init.php
@@ -39,7 +39,7 @@ switch (ENVIRONMENT)
  * 3 = Error and Debug Messages
  * 4 = Error, Debug and Notice Messages
  */
-define('LOGS', 1);
+define('LOGS', 4);
 
 
 include __DIR__.DIRECTORY_SEPARATOR.'vendor/autoload.php';
